91c49fdfea72c6a8854925f7606feef1ec41d5ff
Shellyda <shellydabarbosa@gmail.com>
2021-01-13 20:27:01 -0300

    Feature/adding migrations folder

---
+++ src/migration/1610570350854-CreateUser.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUser1610570350854 implements MigrationInterface {
+    name = 'CreateUser1610570350854'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
